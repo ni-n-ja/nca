93604f92f98ec575740ee2a5dc66fe89145bd3c0
ni_n_ja <rampage4extrm@gmail.com>
2018-07-24 16:50:02 +0900

    a

--- a.js
+++ a.js
@@ -1,19 +1,31 @@
 // Create a new instance of node-core-audio
 var coreAudio = require("node-core-audio");
-    
+
 // Create a new audio engine
 var engine = coreAudio.createNewAudioEngine();
-    
+
 // Add an audio processing callback
 // This function accepts an input buffer coming from the sound card,
 // and returns an ourput buffer to be sent to your speakers.
 //
 // Note: This function must return an output buffer
-    
-function processAudio( inputBuffer ) {    
-    console.log( "%d channels", inputBuffer.length );    
-    console.log( "Channel 0 has %d samples", 
-inputBuffer[0].length );    
+
+function processAudio(inputBuffer) {
+    console.log("%d channels", inputBuffer.length);
+    console.log("Channel 0 has %d samples",
+        inputBuffer[0].length);
     return inputBuffer;
 }
-engine.addAudioCallback( processAudio );
+engine.addAudioCallback(processAudio);
+
+// var engine = coreAudio.createNewAudioEngine();
+
+// // Grab a buffer
+// var buffer = engine.read();
+
+// // Silence the 0th channel
+// for (var iSample = 0; iSample < inputBuffer[0].length; ++iSample)
+//     buffer[0][iSample] = 0.0;
+
+// // Send the buffer back to the sound card
+// engine.write(buffer);
---
+++ b.js
@@ -0,0 +1,16 @@
+var portAudio = require('naudiodon');
+
+console.log(portAudio.getDevices());
+
+var fs = require('fs');
+
+var ai = new portAudio.AudioInput({
+    channelCount: 2,
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 44100,
+    deviceId: 20
+});
+
+ai.on('data', data => console.log(data));
+ai.on('error', err => console.error);
+ai.start();
